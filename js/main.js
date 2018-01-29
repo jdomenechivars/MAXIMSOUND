e5cb8784a58dc9b2da2fdfaa71cedaea62eb7215
joan <jdomenechivars@gmail.com>
2018-01-29 12:23:37 +0100

    add social media links & style get in touch section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,46 @@
-window.onscroll = function() {scrollFunction()};
+/*eslint-env browser*/
+/*eslint "no-console": "off"*/
+/*global $ */
+
+$(document).ready(function () {
+
+  window.onscroll = function () {
+    scrollFunction()
+  };
+
+  styleButtons();
+
+});
+
 
 function scrollFunction() {
 
-    if (document.body.scrollTop > 55 || document.documentElement.scrollTop > 55) {
-        document.getElementById("bar").style.width = "100%";
-        document.getElementById("bar").style.position = "fixed";
-        document.getElementById("bar").style.top = "0";
-    } else {
-        document.getElementById("bar").style.width = "70%";
-        document.getElementById("bar").style.position = "absolute";
-        document.getElementById("bar").style.top = "";
-    }
+  var bar = document.getElementById("bar");
+
+
+  if (document.body.scrollTop > 55 || document.documentElement.scrollTop > 55) {
+    bar.style.width = "100%";
+    bar.style.position = "fixed";
+    bar.style.top = "0";
+  } else {
+    bar.style.width = "70%";
+    bar.style.position = "absolute";
+    bar.style.top = "";
+  }
 }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function styleButtons() {
+
+  $("#getintouch").mouseenter(function () {
+
+    $("#getintouch").css({"background-color": "white","color": "black"})
+
+  });
+
+   $("#getintouch").mouseleave(function () {
+
+    $("#getintouch").css({"background-color": "transparent","color": "white"})
+
+  });
+
 }
