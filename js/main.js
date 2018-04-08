d0eefb898d27ffdfa33bc5269ac5be62cc6a6c7f
joan <jdomenechivars@gmail.com>
2018-04-08 22:39:42 +0200

    add video index style and add trabajos page content and style

--- js/main.js
+++ js/main.js
@@ -54,3 +54,44 @@ function styleButtons() {
   });
 
 }
+
+
+function sendEmail() {
+
+  var name = document.getElementById("jform_contact_name");
+  var mail = document.getElementById("jform_contact_email");
+  var destino = "jdomenechivars@gmail.com";
+  var subject = document.getElementById("jform_contact_emailmsg");
+  var message = document.getElementById("jform_contact_message")
+
+
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("capishi");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
